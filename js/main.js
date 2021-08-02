1c25d2988d2a269a563a87444fbd80faff866f9d
dumplinger <milena.d.industrial@gmail.com>
2021-08-02 19:58:42 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+        let resultado = prompt("Resuelve la siguiente ecuación --> 5x + 9 = 24 <-- ¿Cuál es el valor de x?")
+        
+        let resultErroneo = (parseInt(resultado))
+
+        if (resultado == "3") {
+            alert("Respuesta correcta 🤓")
+            document.write("Eres un genio matemático");
+        }
+
+        else if (resultado == "") {
+            alert("No ingresaste ningún resultado 😱")
+        }
+
+        else {
+            alert("Chequea de nuevo tu cálculo! La respuesta no es " + resultErroneo + "🧐")
+            let reintento = prompt("Intentalo de nuevo --> 5x + 9 = 24 <-- ¿Cuál es el valor de x?")
+            if (reintento == "3") {
+                alert("Ahora si! Respuesta correcta 🤓");
+                document.write("Eres un genio matemático");
+            }
+            else {
+                alert("Lo siento, has fallado de nuevo 😭")
+                document.write("La próxima sale!")
+            }
+        }
